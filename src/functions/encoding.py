6daeac5c9e1578b1f40d37c064f3ebb68fe77afb
Amol Patil <patilketaki12@gmail.com>
2025-04-30 12:35:27 +0200

    Changes for encoding,prdict, split

--- src/functions/encoding.py
+++ src/functions/encoding.py
@@ -1,1 +1,21 @@
 # removes all non-numeric columns Except the city column
+
+import pandas as pd
+def removal_nonnumeric_columns(df:pd.DataFrame)-> pd.DataFrame:
+    """
+    This fucntion removes the non- numeric (except city)columns as not required.
+
+    Parameters:
+    df (pd.DataFrame): The input DataFrame.
+
+    Returns:output dataframe
+    """
+    # Column to keep (even if non-numeric)
+    city_column = 'city'
+
+    # Filter numeric columns and add the keep_column
+    numeric_df = df.select_dtypes(include='number')  # keeps only numeric columns
+    if city_column in df.columns:
+        numeric_df[city_column] = df[city_column]
+    return numeric_df
+
--- src/functions/predict.py
+++ src/functions/predict.py
@@ -1,1 +1,32 @@
 # predicts cases on validation set
+import pandas as pd
+
+def make_prediction(df: pd.DataFrame, model)=>pd.series:
+    """
+    This fucntion accepts the train/test data and make the prediction based on the model 
+    Parameters:
+    df (pd.DataFrame): The input DataFrame.
+    model (Data model): The model used for prediction
+
+    Returns:output dataframe(pd.DataFrame)
+    """
+    """
+    Predict using a model and a subset of features from the DataFrame.
+
+    Args:
+        model: Trained model with a .predict() method (e.g., sklearn model).
+        df (pd.DataFrame): Input data.
+        feature_columns (list): List of column names to use as features.
+
+    Returns:
+        pd.Series: Model predictions.
+    """
+    # Ensure all required columns are in the DataFrame
+    #missing = set(feature_columns) - set(df.columns)
+    #if missing:
+       # raise ValueError(f"Missing required columns: {missing}")
+
+    #X = df[feature_columns]
+    X=df
+    preds = model.predict(X)
+    return pd.Series(preds, index=df.index, name='Prediction')
