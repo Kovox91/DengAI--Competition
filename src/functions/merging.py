28d4fff721d275623ee834dcb3bace48eae47cdc
FrancescaCapuano <f.capuano1991@gmail.com>
2025-04-30 12:08:49 +0200

    added basic merging function

--- src/functions/merging.py
+++ src/functions/merging.py
@@ -1,1 +1,14 @@
-# merges the outcomes from loading into a single dataframe
+import pandas as pd
+
+def merge(df1, df2, on):
+    """
+    Merge two DataFrames on common columns.
+
+    Args:
+        df1, df2 : The two DataFrames to merge.
+        on (list): Column(s) to merge on.
+
+    Returns:
+        pd.DataFrame: Merged DataFrame.
+    """    
+    return pd.merge(df1, df2, on=on)
