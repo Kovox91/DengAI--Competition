88dee81ad354918e1a4a28807e36b593a89cd87f
FrancescaCapuano <f.capuano1991@gmail.com>
2025-04-30 16:17:37 +0200

    changed model to regressor

--- src/functions/train.py
+++ src/functions/train.py
@@ -1,17 +1,17 @@
 import pandas as pd
-import lightgbm as lgb
+from lightgbm import LGBMRegressor
 
-def train_model(X_train:pd.DataFrame, y_train:pd.DataFrame, learning_rate=.09, max_depth=15, num_leaves=10):
+def train_model(X_train:pd.DataFrame, y_train:pd.DataFrame, learning_rate=.01, max_depth=15, num_leaves=10):
     """
     Trains a linear regression model on the training data.
     Args:
         X_train (pd.DataFrame): The training data features.
         y_train (pd.DataFrame): The training data target variable.
     Returns:
-        model (RegLGBMClassifier): The trained linear regression model.
+        model (LGBMRegressor): The trained linear regression model.
     """
     # initialize the model
-    model = lgb.LGBMClassifier(learning_rate=learning_rate,max_depth=max_depth,num_leaves=num_leaves,random_state=42)
+    model = LGBMRegressor(learning_rate=learning_rate,max_depth=max_depth,num_leaves=num_leaves,random_state=42)
 
     # fit
     model.fit(X_train,y_train)
