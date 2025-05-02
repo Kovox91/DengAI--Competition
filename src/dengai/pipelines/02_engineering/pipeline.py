b450e73f9039c9187d39e72b0da726ec82451d5f
Kovox91 <sascha.maschmann@hotmail.de>
2025-05-02 11:44:18 +0200

    Added fav_conditions

--- src/dengai/pipelines/02_engineering/pipeline.py
+++ src/dengai/pipelines/02_engineering/pipeline.py
@@ -21,8 +21,13 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="lag_features_added",
             ),
             node(
+                func=add_favourable_conditions,
+                inputs=["lag_features_added", "params:temperature_threshholds"],
+                outputs="fav_temps_added",
+            ),
+            node(
                 func=removal_nonnumeric_columns,
-                inputs="imputed_data_mean",
+                inputs="fav_temps_added",
                 outputs="numeric_data",
             ),
             node(
